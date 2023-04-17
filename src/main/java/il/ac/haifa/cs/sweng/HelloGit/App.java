9036cbbe39e8c854667347c83fad0c9103acd0e3
reman <reman>
2023-04-17 21:31:02 +0300

    hi yaman

--- src/main/java/il/ac/haifa/cs/sweng/HelloGit/App.java
+++ src/main/java/il/ac/haifa/cs/sweng/HelloGit/App.java
@@ -28,6 +28,7 @@ public class App extends Application {
 
     public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        ;
         return fxmlLoader.load();
     }
 
